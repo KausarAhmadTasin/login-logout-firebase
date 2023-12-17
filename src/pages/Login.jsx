4d61ac2443a1a3fb9690cb3479f2530acad12842
KausarAhmadTasin <kausarahmadtasinkat@gmail.com>
2023-12-17 22:07:14 +0600

    second

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,5 @@
-import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../config/firebase";
+
 const Login = () => {
   return (
     <section className="bg-gray-50">
@@ -70,7 +68,6 @@ const Login = () => {
               </div>
               <button
                 type="submit"
-                onClick={() => logIn}
                 className="w-full bg-green-200 bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
               >
                 Log in
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -10,7 +10,6 @@ const Signup = () => {
   const signUp = async () => {
     await createUserWithEmailAndPassword(auth, email, password);
   };
-  //   console.log(email);
   return (
     <section className="bg-gray-50">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
